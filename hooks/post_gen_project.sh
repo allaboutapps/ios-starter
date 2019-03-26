ca3069399858f96b72b9b5c03fcee9bc0003b804
Oliver Ian Krakora <oliver.krakora@allaboutapps.at>
2019-03-26 14:39:50 +0100

    resolves #2 #3 #4

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,8 +1,28 @@
 #! /bin/bash
 
-xcodegen
-carthage update --platform ios --cache-builds
-cd '{{cookiecutter.projectName}}-ios'
-xed .
+echo "Fetching strings"
+./buildStrings
+
+echo "Create xcode project file now? : y/n "
+
+read XCODE_GEN
+
+if [ "$XCODE_GEN" == "y" ];then
+  xcodegen
+fi
+
+echo "Perform carthage update now? : y/n "
+
+read RUN_CARTHAGE 
+
+if [ "$RUN_CARTHAGE" == "y" ];then
+  carthage update --platform ios --cache-builds
+fi
+
+pwd
+
+if [ "$XCODE_GEN" == "y" ];then
+  xed .
+fi
 
 printf 'all done - enjoy \xf0\x9f\x9a\x80\n'
